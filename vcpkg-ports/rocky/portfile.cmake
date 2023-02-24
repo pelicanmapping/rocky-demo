8726ba348f5044a9a06f1b83619d0bc249308440
Paul Levy <plevy@pelicanmapping.com>
2023-02-24 15:09:26 -0500

    fix toolchain, vsg SHA512, and use head from rocky so no SHA needed

--- vcpkg-ports/rocky/portfile.cmake
+++ vcpkg-ports/rocky/portfile.cmake
@@ -1,8 +1,7 @@
+set(VCPKG_USE_HEAD_VERSION ON)
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO pelicanmapping/rocky
-    SHA512 81d59a0b804e4b16581adb6280f62065fcb57f9788418fe47b670e748287bc4b122b54a4cbe9db5feb5ab1c050d7a5652f9fbd114a6a8719765ac9ae13dce81f
-    REF main
 	HEAD_REF main
 )
 
