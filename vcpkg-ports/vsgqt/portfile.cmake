ff95187ef32e352175c8e118f904e750685213ef
Glenn Waldron <gwaldron@gmail.com>
2024-03-19 10:45:55 -0400

    Update vcpkg ports

--- vcpkg-ports/vsgqt/portfile.cmake
+++ vcpkg-ports/vsgqt/portfile.cmake
@@ -1,6 +1,6 @@
 #vcpkg_from_github(
 #	OUT_SOURCE_PATH SOURCE_PATH
-#	REPO vsg-dev/vsgImGui
+#	REPO vsg-dev/vsgQt
 #    SHA512 969ae0cf67aff48d4846007f5301a0c302493162408412f88836b218bbf83c493e9eb63f8b668eda6363580650b8b7d01c9991c25f2d7109c0b43379c1e219b2
 #    REF ba24c5776414129d8631d8b6fe05f8860bb8566f
 #	HEAD_REF master
@@ -8,8 +8,8 @@
 
 find_program(GIT git)
 
-set(GIT_URL "https://github.com/vsg-dev/vsgImGui.git")
-set(GIT_REV "f32467462850eb38ce0d003dbd6960259e09b16e")
+set(GIT_URL "https://github.com/vsg-dev/vsgQt.git")
+set(GIT_REV "2d048be26c5a6dbe2e61290d0bb2611d75c79547")
 
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
 
@@ -33,7 +33,7 @@ vcpkg_execute_required_process(
 
 vcpkg_apply_patches(
    SOURCE_PATH "${SOURCE_PATH}"
-   PATCHES "vsgImGui.patch"
+   PATCHES "glslang.patch"
 )
 
 vcpkg_configure_cmake(
@@ -42,7 +42,7 @@ vcpkg_configure_cmake(
 
 
 vcpkg_install_cmake()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "vsgimgui" CONFIG_PATH "lib/cmake/vsgimgui")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "vsgqt" CONFIG_PATH "lib/cmake/vsgqt")
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
